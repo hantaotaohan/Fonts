bd82336c61e7833df1238c3cbe07c2340ca65e0c
TaoTao <hantaotaohan@gmail.com>
2019-12-10 11:54:17 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,55 +2,42 @@
 #!/bin/sh
 
 
+#-------------------------------------------------------------------
+
+# Setting Colors
+
+#-------------------------------------------------------------------
+
+blue='\e[0;34m'
+green='\e[0;32m'
 
 # Set source and target directories
 
 powerline_fonts_dir="$( cd "$( dirname "$0" )" && pwd )"
 
-
-
 # if an argument is given it is used to select which fonts to install
 
 prefix="$1"
 
-
-
 if test "$(uname)" = "Darwin" ; then
-
   # MacOS
-
   font_dir="$HOME/Library/Fonts"
-
 else
-
   # Linux
-
   font_dir="$HOME/.local/share/fonts"
-
   mkdir -p $font_dir
-
 fi
-
-
 
 # Copy all fonts to user fonts directory
 
-echo "Copying fonts..."
-
+echo -e "\t[+]${blue}Copying fonts..."
 find "$powerline_fonts_dir" \( -name "$prefix*.[ot]tf" -or -name "$prefix*.pcf.gz" \) -type f -print0 | xargs -0 -n1 -I % cp "%" "$font_dir/"
-
-
 
 # Reset font cache on Linux
 
 if which fc-cache >/dev/null 2>&1 ; then
-
-    echo "Resetting font cache, this may take a moment..."
-
+    echo -e "\t[+]${blue}Resetting font cache, this may take a moment..."
     fc-cache -f "$font_dir"
-
 fi
 
-
-
-echo "Powerline fonts installed to $font_dir"
+echo -e "\t[+]${blue}Powerline fonts installed to $font_dir"
