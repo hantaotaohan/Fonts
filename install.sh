ac5e1dc4ad462e2508db9e7a980ca99786916079
hantaotaohan <hantaotaohan@gmail.com>
2022-01-14 09:22:36 +0800

    first commit

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Set source and target directories
+powerline_fonts_dir="$( cd "$( dirname "$0" )" && pwd )"
+
+# if an argument is given it is used to select which fonts to install
+prefix="$1"
+
+if test "$(uname)" = "Darwin" ; then
+  # MacOS
+  font_dir="$HOME/Library/Fonts"
+else
+  # Linux
+  font_dir="$HOME/.local/share/fonts"
+  mkdir -p $font_dir
+fi
+
+# Copy all fonts to user fonts directory
+echo -e "              [#] Copying Fonts......\n"
+find "$powerline_fonts_dir" \( -name "$prefix*.[ot]tf" -or -name "$prefix*.pcf.gz" \) -type f -print0 | xargs -0 -n1 -I % cp "%" "$font_dir/"
+
+# Reset font cache on Linux
+if which fc-cache >/dev/null 2>&1 ; then
+    echo -e "              [#] Resetting font cache, this may take a moment...\n"
+    fc-cache -f "$font_dir"
+fi
+
+cd $HOME
+echo -e "              [#] Powerline fonts installed to $font_dir\n"
